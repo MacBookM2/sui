07de3f4d5e19f335f4e036626cd26bfd7c09ce5e
Ashok Menon <ashok@mystenlabs.com>
2026-01-07 13:40:06 +0000

    deps: migrate from rustls-pemfile to rustls-pki-types
    Replace direct usage of rustls-pemfile with the PemObject trait from
    rustls-pki-types, which is the recommended migration path per
    RUSTSEC-2025-0134 (rustls-pemfile is now unmaintained).
    
    Affected crates:
    - sui-pg-db: use CertificateDer::pem_slice_iter() for CA cert parsing
    - sui-proxy: use CertificateDer::pem_file_iter() and
      PrivateKeyDer::from_pem_file() for TLS cert/key loading
    - sui-types: use CertificateDer::pem_slice_iter() for embedded root cert

--- crates/sui-pg-db/src/tls.rs
+++ crates/sui-pg-db/src/tls.rs
@@ -17,6 +17,7 @@ use rustls::client::danger::ServerCertVerifier;
 use rustls::pki_types::CertificateDer;
 use rustls::pki_types::ServerName;
 use rustls::pki_types::UnixTime;
+use rustls::pki_types::pem::PemObject;
 use tokio_postgres_rustls::MakeRustlsConnect;
 use tracing::error;
 use webpki_roots::TLS_SERVER_ROOTS;
@@ -124,7 +125,7 @@ pub(crate) fn build_tls_config(
         })?;
 
         let certs = if ca_cert_bytes.starts_with(b"-----BEGIN CERTIFICATE-----") {
-            rustls_pemfile::certs(&mut ca_cert_bytes.as_slice())
+            CertificateDer::pem_slice_iter(&ca_cert_bytes)
                 .collect::<Result<Vec<_>, _>>()
                 .with_context(|| {
                     format!(
