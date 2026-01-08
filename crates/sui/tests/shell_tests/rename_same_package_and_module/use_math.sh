db1a14c2420e142980df6d1b44ece8ed5e340513
Manos Liolios <emmanouil.liolios@gmail.com>
2026-01-08 14:26:59 +0200

    Add shell test that we can use + tst both packages named the same

---
+++ crates/sui/tests/shell_tests/rename_same_package_and_module/use_math.sh
@@ -0,0 +1,8 @@
+# Copyright (c) Mysten Labs, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+# tests that building a package that uses two packages that both define their name as "math" works.
+sui move --client.config $CONFIG build -p use_math
+
+# tests that tests also work for the same scenario
+sui move --client.config $CONFIG test -p use_math
---
+++ crates/sui/tests/shell_tests/rename_same_package_and_module/use_math/tests/test.move
@@ -0,0 +1,8 @@
+#[test_only]
+module use_math::testing;
+
+#[test]
+fun use_test() {
+    std::debug::print(&math_a::math::a());
+    std::debug::print(&math_b::math::a());
+}
